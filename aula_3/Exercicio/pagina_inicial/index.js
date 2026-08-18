d3d8bb93e651c56db4fcbfe8b7f6e9ffb462464b
ViniNovack <vininomn@gmail.com>
2026-08-18 18:42:17 -0300

    continuação

---
+++ aula_3/Exercicio/pagina_inicial/index.js
@@ -0,0 +1,6 @@
+const botao1 = document.getElementById("botao1")
+const botao2 = document.getElementById("botao2")
+
+botao1.onclick = function(){
+    window.location.href = "../calculadora/index.html";
+}
